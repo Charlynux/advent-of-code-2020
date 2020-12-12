a752d482a34ed97f423b17d91667e8ec8c2a889a
Charles Fourdrignier <charles.fourdrignier@gmail.com>
2020-12-12 08:09:48 +0100

    Day 12 - Part 2

--- day12/day12.clj
+++ day12/day12.clj
@@ -20,12 +20,21 @@
   ([n direction position] (move (mapv #(* n %) direction) position)))
 
 (defn turn-right [angle direction]
-  (let [index (.indexOf DIRECTIONS direction)
-        offset (/ angle 90)]
-    (nth DIRECTIONS (mod (+ index offset) (count DIRECTIONS)))))
+  (let [offset (/ angle 90)
+        turn (fn [[x y]] [y (- x)])]
+    (nth (iterate turn direction) offset)))
 
 (defn turn-left [angle direction]
-  (turn-right (- angle) direction))
+  (let [offset (/ angle 90)
+        turn (fn [[x y]] [(- y) x])]
+    (nth (iterate turn direction) offset)))
+
+(turn-right 90 [1 0])
+;; [0 -1]
+(turn-right 90 [10 4])
+;; [4 -10]
+
+(turn-left 90 [1 0])
 
 (defn apply-actions [{:keys [position direction] :as state} [action value]]
   (cond
@@ -60,3 +69,28 @@
 
 (solve-part-1 sample-input)
 (solve-part-1 real-input)
+
+(defn apply-actions-2 [{:keys [position waypoint] :as state} [action value]]
+  (cond
+    ;; move waypoint
+    (#{:N :S :E :W} action) (update state :waypoint #(move value (action->direction action) %))
+    (= action :R) (update state :waypoint #(turn-right value %))
+    (= action :L) (update state :waypoint #(turn-left value %))
+    ;; move ship
+    (= action :F) (update state :position #(move value waypoint %))
+    :else state))
+
+(def initial-state-2 {:waypoint [10 1] :position [0 0]})
+
+(reduce apply-actions-2 initial-state-2 [[:F 10] [:N 3] [:F 7] [:R 90]])
+
+(defn solve-part-2 [input]
+  (->>
+   input
+   parse-input
+   (reduce apply-actions-2 initial-state-2)
+   :position
+   (manhattan-distance [0 0])))
+
+(solve-part-2 sample-input)
+(solve-part-2 real-input)
