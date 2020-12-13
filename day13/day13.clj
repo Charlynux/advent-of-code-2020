6d3a33b0a615b0fd4dc4ddd7ca9e7a8ea9e08562
Charles Fourdrignier <charles@iteracode.fr>
2020-12-13 21:58:28 +0100

    Day 13 - Part 2 solution

--- day13/day13.clj
+++ day13/day13.clj
@@ -40,14 +40,95 @@
 (defn valid? [n pairs]
   (every? (fn [[value index]] (zero? (mod (+ index n) value))) pairs))
 
-(time (let [input sample-input
-           parsed-input (parse-input input)
-           pairs (->> parsed-input
-                      second
-                      vec
-                      (map-indexed pair)
-                      (remove nil?))
-           max-pair (last (sort-by first pairs))
-           steps (map (step max-pair) (drop 1 (range)))]
+#_(time (let [input sample-input
+            parsed-input (parse-input input)
+            pairs (->> parsed-input
+                       second
+                       vec
+                       (map-indexed pair)
+                       (remove nil?))
+            max-pair (last (sort-by first pairs))
+            steps (map (step max-pair) (drop 1 (range)))]
         (first (filter #(valid? % pairs) steps))))
 ;; 20 seconds, with sample-input !!!!!!!
+(require '[clojure.set :as set])
+
+(defn find-meets
+  "Debug function..."
+  [[a x] [b y]]
+  (let [meets (set/intersection
+               (set (take 100 (iterate (partial + a) x)))
+               (set (take 100 (iterate (partial + b) y))))]
+    (prn meets)
+    (prn "Min :" (apply min meets))
+    (prn "Dist :" (map #(apply - %) (partition 2 1 (sort > meets))))))
+
+(find-meets [7 0] [17 -1])
+(find-meets [17 0] [13 -2])
+(find-meets [19 3] [221 102])
+
+;; modulo "all meets" distance = min meet of the two
+(mod 1068781 91)
+
+(defn gcd
+  [a b]
+  (loop [a a
+         b b]
+    (if (zero? a)
+      b
+      (recur (mod b a) a))))
+(gcd 15 25)
+(gcd 13 7)
+
+(defn lcm [a b]
+  (* (/ a (gcd a b)) b))
+(lcm 15 25)
+(lcm 13 7) ;; 91
+
+(defn read-pairs [parsed]
+  (->> parsed
+       second
+       vec
+       (map-indexed pair)
+       (remove nil?)))
+
+(defn foo
+  ([a-pair] a-pair)
+  ([[a offset-a] [b offset-b]]
+   (prn [a offset-a] [b offset-b])
+   [(lcm a b)
+    (time (+ (first (filter #(zero? (mod (- % (- offset-b offset-a)) b)) (iterate (partial + a) 0))) offset-a))]))
+
+;; A pair store 2 informations :
+;; - the step's length (when the bus get back to start point)
+;; - the offset from first bus
+;; The `foo` function "merges" two bus in a new one.
+;; The new bus arrives at start point when the two merged bus arrived.
+;; So step length = lcm(step lengths) and offset = first meeting of the two bus.
+;;
+;; This implies that you can call `foo` with a merged bus obtaining a bigger bus.
+;; We do this until we get "full bus" merging all buses.
+
+(defn solve-part-2 [input]
+  (let [init-pairs (read-pairs (parse-input input))]
+    (apply - (loop [pairs init-pairs]
+               (if (= (count pairs) 1)
+                 (first pairs)
+                 (recur (map #(apply foo %) (partition-all 2 (sort-by second pairs)))))))))
+
+(solve-part-2 "IGNORED\n17,x,13,19")
+;; 3417
+(solve-part-2 "IGNORED\n67,7,59,61")
+;; 754018
+(solve-part-2 "IGNORED\n67,x,7,59,61")
+;; 779210
+(solve-part-2 "IGNORED\n67,7,x,59,61")
+;; 1261476
+(solve-part-2 "IGNORED\n1789,37,47,1889")
+;; 1202161486
+
+(solve-part-2 sample-input)
+;; 1068781
+
+(time (solve-part-2 real-input))
+;; 836024966345345
