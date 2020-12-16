8623e26a628bfd9eef516af9d3b0e43bb5bc48cc
Charles Fourdrignier <charles.fourdrignier@gmail.com>
2020-12-16 09:18:35 +0100

    Day 16 - Part 2

--- day16/day16.clj
+++ day16/day16.clj
@@ -1,4 +1,5 @@
-(require '[clojure.string :as str])
+(require '[clojure.string :as str]
+         '[clojure.set :as set])
 
 (def sample-input "class: 1-3 or 5-7
 row: 6-11 or 33-44
@@ -12,6 +13,18 @@ nearby tickets:
 40,4,50
 55,2,20
 38,6,12")
+
+(def sample-input-2 "class: 0-1 or 4-19
+row: 0-5 or 8-19
+seat: 0-13 or 16-19
+
+your ticket:
+11,12,13
+
+nearby tickets:
+3,9,18
+15,1,5
+5,14,9")
 
 (def real-input (slurp "day16/input"))
 
@@ -31,6 +44,7 @@ nearby tickets:
 (defn parse-input [input]
   (let [[classes ticket nearby] (str/split input #"\R\R")]
     {:classes (map parse-class (str/split-lines classes))
+     :ticket (first (map parse-ticket (drop 1 (str/split-lines ticket))))
      :nearby (map parse-ticket (drop 1 (str/split-lines nearby)))}))
 
 (parse-input sample-input)
@@ -51,3 +65,41 @@ nearby tickets:
 
 (solve-part-1 sample-input)
 (solve-part-1 real-input)
+
+(defn valid-class? [n [_ ranges]]
+  (some
+   (fn [[low high]] (<= low n high))
+   ranges))
+
+(defn find-classes [classes ticket]
+  (for [n ticket]
+    (set (map first (filter #(valid-class? n %) classes)))))
+
+(defn clean-classes [classes]
+  (loop [classes classes]
+    (let [sures (into #{} (comp (filter #(= (count %) 1)) cat) classes)
+          new-classes (map #(if (= 1 (count %))
+                              %
+                              (set/difference % sures)) classes)]
+      (if (= classes new-classes)
+        new-classes
+        (recur new-classes)))))
+
+(clean-classes '(#{:row :seat} #{:seat :class} #{:class}))
+
+(defn get-ticket-map [{:keys [classes ticket nearby]}]
+  (let [pos-classes (->>
+                     nearby
+                     (map #(find-classes classes %))
+                     (remove #(some empty? %))
+                     (cons (find-classes classes ticket))
+                     (apply map set/intersection))
+        pos->class (map first (clean-classes pos-classes))]
+    (zipmap pos->class ticket)))
+
+(get-ticket-map (parse-input sample-input-2))
+
+(let [ticket (get-ticket-map (parse-input real-input))
+      ks (filter #(.startsWith (name %) "departure") (keys ticket))]
+  (apply * (map ticket ks)))
+;; => 279139880759
