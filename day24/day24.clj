bd53b6fdff8c92e524884d50a78fc465895e7eb8
Charles Fourdrignier <charles.fourdrignier@gmail.com>
2020-12-26 09:07:00 +0100

    Day 24 - Part 2

--- day24/day24.clj
+++ day24/day24.clj
@@ -53,9 +53,35 @@ wseweeenwnesenwwwswnew")
           (map #(move REFERENCE_TILE %)
                (parse-input input))))
 
-(def initial-layout (create-matrix (slurp "day24/input")))
+(defn keep-black-tiles [matrix]
+  (into #{} (keep #(when (= 1 (second %)) (first %)) matrix)))
 
-(def black-tiles (filter (comp #{1} second) initial-layout))
+
+(defn find-initial-layout [input]
+  (keep-black-tiles (create-matrix input)))
 
 ;; Part 1
-(count black-tiles)
+(count (find-initial-layout (slurp "day24/input")))
+
+;; "Game of Life Rules"
+;; - Any black tile with zero or more than 2 black tiles immediately adjacent to it is flipped to white.
+;; - Any white tile with exactly 2 black tiles immediately adjacent to it is flipped to black.
+
+(defn neighbors [coord]
+  (map #(mapv + % coord) (vals dir->move)))
+
+(defn generation [black-tiles]
+  (let [nb-neighbors (frequencies (mapcat neighbors black-tiles))]
+    (into #{}
+          (keep (fn [[coord nb]] (cond
+                                   (and (black-tiles coord)
+                                        (<= 1 nb 2)) coord
+                                   (and (not (black-tiles coord))
+                                        (= 2 nb)) coord)))
+          nb-neighbors)))
+
+(defn solve-part-2 [input]
+  (count (nth (iterate generation (find-initial-layout input)) 100)))
+
+(solve-part-2 sample-input)
+(solve-part-2 (slurp "day24/input"))
